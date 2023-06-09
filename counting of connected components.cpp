961dc3f56156b31c6a60774635659ed4218f5ac0
@coder shruti <74962507+ShrutiRani7029@users.noreply.github.com>
2023-06-10 00:47:47 +0530

    Create counting of connected components.cpp
    Time Complexity: o(v+e)
    Space Complexity: o(v)
    v: no. of vertex,
    E: no. of edges

---
+++ counting of connected components.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=1e5+10;
+vector<int>g[N];
+bool vis[N];
+
+void dfs(int vertex)
+{
+  vis[vertex]=true;
+  for(int child:g[vertex])
+  {
+    if(vis[child])continue;
+    dfs(child);
+  }
+}
+int main()
+{
+  int n,m;
+  cin>>n>>m;
+  for(int i=0;i<m;i++)
+  {
+    int v1,v2;
+    cin>>v1>>v2;
+    g[v1].push_back(v2);
+    g[v2].push_back(v1);
+  }
+  int count=0;
+  for(int i=1;i<=n;i++)
+  {
+    if(vis[i])continue;
+    dfs(i);
+    count++;
+  }
+  cout<<"Total Connected Components : "<<count;
+}
